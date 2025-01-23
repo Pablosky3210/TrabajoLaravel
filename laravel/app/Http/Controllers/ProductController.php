0b5be1053efd59551f536e8bf898492d792ae640
Pablosky3210 <pg447186@gmail.com>
2025-01-23 17:41:03 +0100

    primer commit

---
+++ laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products= Product::orderBy('created_at','desc')->get();
+        return view('products.index',compact('products'));
+    }
+
+    public function create(){
+        $products= Product::orderBy('created_at','desc')->get();
+        return view('products.create',compact('products'));
+    }
+}
---
+++ laravel/resources/views/products/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista admin</h1>
+    Bienvenido {{auth()->user()->name}}
+    <a href="javascript: document.getElementById('logout').submit()">Cerrar Sesion</a>
+    <form id="logout" action="{{route('logout')}}" method="post" style="display: none;">
+        @csrf
+    </form>
+</body>
+</html>
